1982a77262da6ede7cf1b2388ca8babb73e71c82
Ben Sherman <bennettmsherman@gmail.com>
2017-09-28 10:53:30 -0400

    Remove accidental character

--- src/command/CommandParser.cpp
+++ src/command/CommandParser.cpp
@@ -142,7 +142,7 @@ std::string CommandParser::getCommandStringList() const
         cmdList.append("\n");
     }
 
-    cmdList.append("\nxRTL FM RUNNER COMMANDS: \n");
+    cmdList.append("\nRTL FM RUNNER COMMANDS: \n");
     for (size_t idx = 0; idx < RTL_FM_RUNNER_CMDS_LIST_LENGTH; ++idx)
     {
         cmdList.append(RTL_FM_RUNNER_CMDS[idx].getCommandString());
